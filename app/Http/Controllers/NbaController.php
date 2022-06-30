a1c0765521bb4889ecc8032fa0d1151ee42f92ea
alerocchello <102465333+alerocchello@users.noreply.github.com>
2022-06-30 09:46:27 +0200

    Add files via upload

--- app/Http/Controllers/NbaController.php
+++ app/Http/Controllers/NbaController.php
@@ -14,5 +14,35 @@ class NbaController extends BaseController {
 
         return view("nba")->with("user", $user);
     }
+
+    public function nbaTeams() {
+			$curl = curl_init();
+
+			curl_setopt_array($curl, [
+				CURLOPT_URL => "https://nba-teams2.p.rapidapi.com/NBA",
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_FOLLOWLOCATION => true,
+				CURLOPT_ENCODING => "",
+				CURLOPT_MAXREDIRS => 10,
+				CURLOPT_TIMEOUT => 30,
+				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+				CURLOPT_CUSTOMREQUEST => "GET",
+				CURLOPT_HTTPHEADER => [
+					"X-RapidAPI-Host: nba-teams2.p.rapidapi.com",
+					"X-RapidAPI-Key: 4bfcc78970mshce645f492d886cdp172b44jsn1426c44f320c"
+				],
+			]);
+
+			$response = curl_exec($curl);
+			$err = curl_error($curl);
+
+			curl_close($curl);
+
+			if ($err) {
+				return "cURL Error #:" . $err;
+			} else {
+				return $response;
+			}
+    }
 }
 ?>
