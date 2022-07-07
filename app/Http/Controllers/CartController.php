1cb091e4816834065015dc4ba59d5712b6d9e2b2
alerocchello <102465333+alerocchello@users.noreply.github.com>
2022-07-07 21:31:48 +0200

    Add files via upload

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -20,6 +20,12 @@ class CartController extends BaseController {
     }
 
     public function addToCart($id) {
+        $cart = new Cart;
+        $cart->username_utente = session('username');
+        $cart->id_prodotto = $id;
+        $cart->save();
+        /*
+        // Carrello con num
         $cart = Cart::where('username_utente', session('username'))
                       ->where('id_prodotto', $id)
                       ->first();
@@ -34,26 +40,32 @@ class CartController extends BaseController {
             $cart->num = 1;
             $cart->save();
         }
+        */
     }
 
     public function removeFromCart($id) {
         $cart = Cart::where('username_utente', session('username'))
                     ->where('id_prodotto', $id)
                     ->first();
+        $cart->delete();
 
+        /*
+        // Carrello con num
         if($cart->num > 1) {
             $cart->num = $cart->num -1;
             $cart->save();
         } else {
             $cart->delete();
         }
+        */
     }
 
     public function returnCart() {
         $id_tshirts = DB::table('carts')
                         ->where('username_utente', session('username'))
                         ->orderBy('id_prodotto')
-                        ->pluck('id_prodotto'); //restituisce solo i valori di una colonna
+                        ->pluck('id_prodotto'); //restituisce solo i valori di una colonna e li mette in un array[]
+
 
         foreach ($id_tshirts as $id_tshirt) {
             $tshirts[] = Tshirt::find($id_tshirt);
